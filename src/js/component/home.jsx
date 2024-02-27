2c46d941fe5c6f58818ae082a3e06a1b35fe12e2
brandenreilly <155998782+brandenreilly@users.noreply.github.com>
2024-02-27 20:59:38 +0000

    added functionality to glow change + a cycle button + autocycle

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
+	const [style, setStyle] = useState("redglow");
+	const autoCycle = () => {
+		if (style == "yellowglow"){
+			setStyle("redglow")
+		}else if(style == "redglow"){
+			setStyle("greenglow")
+		}else if(style == "greenglow"){
+			setStyle("yellowglow")
+		}
+	}
+	setTimeout(autoCycle, 1000)
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container-fluid">
+			<div className="d-flex justify-content-center">
+				<div className="trafficlight-body">
+					<div className={style == "redglow" ? "glow": ""} id="firstlight" onClick={() => {
+						console.log("Clicked on red")
+						if (style !== "redglow") setStyle("redglow");
+					}}>
+
+					</div>
+					<div className={style == "yellowglow" ? "glow": ""} id="secondlight" onClick={() => {
+					console.log("clicked on yellow")
+					if (style !== "yellowglow") setStyle("yellowglow");
+					}}>
+					</div>
+					<div className={style == "greenglow" ? "glow": ""} id="thirdlight" onClick={() => {
+					console.log("clicked on green")
+						if (style !== "greenglow") setStyle("greenglow");
+					}}>
+					</div>
+				</div>
+			</div>
+			<div className="d-flex justify-content-center">
+					<button className="btn btn-primary btn-lg" onClick={() => {
+						if (style == "yellowglow"){
+							setStyle("redglow")
+						}else if(style == "redglow"){
+							setStyle("greenglow")
+						}else if(style == "greenglow"){
+							setStyle("yellowglow")
+						}
+					}}>Cycle Lights</button>
+			</div>
 		</div>
 	);
 };
